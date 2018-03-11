0d96ca9a3f9610da9f1808787ee4be74773c266e
Sergey Elsukov <oct8cat@gmail.com>
2018-03-11 04:24:20 +0300

    Prettify

--- bin/start.js
+++ bin/start.js
@@ -1,19 +1,24 @@
-const {createDb, connectDb, disconnectDb} = require('../lib/db')
-const {stopServer, createApp, createServer, startServer} = require('../lib/http')
+const { createDb, connectDb, disconnectDb } = require('../lib/db')
+const {
+  stopServer,
+  createApp,
+  createServer,
+  startServer
+} = require('../lib/http')
 
-const start = exports.start = () => {
+const start = (exports.start = () => {
   const db = createDb()
-  const app = createApp({db})
+  const app = createApp({ db })
   const server = createServer(app)
   return connectDb(db)
     .then(() => startServer(server))
-    .then((server) => ({db, app, server}))
-}
+    .then(server => ({ db, app, server }))
+})
 
-exports.stop = ({db, server, ...rest} = {}) => {
+exports.stop = ({ db, server, ...rest } = {}) => {
   return stopServer(server)
     .then(() => disconnectDb(db))
-    .then(() => ({db, server, ...rest}))
+    .then(() => ({ db, server, ...rest }))
 }
 
 if (require.main === module) start()
--- test/index.js
+++ test/index.js
@@ -3,14 +3,31 @@ const supertest = require('supertest')
 const path = require('path')
 const assert = require('assert')
 const WebSocket = require('ws')
-const {clearDb, userModel, connectDb, disconnectDb, createDb} = require('../lib/db')
-const {stopWSServer, stopServer, createWSServer, startServer, createServer, createApp} = require('../lib/http')
-const {exec, schema, requestStrings: {uploadsString, meString}} = require('../lib/graphql')
-const {generateUserToken} = require('../lib/util')
-const {HOST, PORT} = require('../lib/const')
+const {
+  clearDb,
+  userModel,
+  connectDb,
+  disconnectDb,
+  createDb
+} = require('../lib/db')
+const {
+  stopWSServer,
+  stopServer,
+  createWSServer,
+  startServer,
+  createServer,
+  createApp
+} = require('../lib/http')
+const {
+  exec,
+  schema,
+  requestStrings: { uploadsString, meString }
+} = require('../lib/graphql')
+const { generateUserToken } = require('../lib/util')
+const { HOST, PORT } = require('../lib/const')
 
 const db = createDb()
-const ctx = {db}
+const ctx = { db }
 const app = createApp(ctx)
 const req = supertest(app)
 
@@ -21,12 +38,12 @@ describe('As anonymous', () => {
   it('Authorize with JWT', () => {
     return clearDb(db)
       .then(() => userModel(db).create({}))
-      .then((user) => {
+      .then(user => {
         return req
           .post('/graphql')
           .set('authorization', `Bearer ${generateUserToken(user)}`)
-          .send({query: meString})
-          .then(({body: {data: {me}}}) => {
+          .send({ query: meString })
+          .then(({ body: { data: { me } } }) => {
             assert.equal(me.id, user.id)
           })
       })
@@ -35,12 +52,11 @@ describe('As anonymous', () => {
     let server, wsServer
     beforeEach(() => {
       server = createServer()
-      wsServer = createWSServer({server})
+      wsServer = createWSServer({ server })
       return startServer(server)
     })
     afterEach(() => {
-      return stopWSServer(wsServer)
-        .then(() => stopServer(server))
+      return stopWSServer(wsServer).then(() => stopServer(server))
     })
     it('Connect to public web socket', () => {
       return new Promise((resolve, reject) => {
@@ -57,36 +73,37 @@ describe('As authorized user', () => {
   let user
   beforeEach(() => {
     return clearDb(db)
-      .then(() => userModel(db).create({displayName: 'authorized user'}))
-      .then((user_) => (user = user_))
+      .then(() => userModel(db).create({ displayName: 'authorized user' }))
+      .then(user_ => (user = user_))
   })
   it('View my profile', () => {
-    return exec(schema, meString, {}, {user, ...ctx})
-      .then(({errors, data}) => {
-        if (errors) return Promise.reject(Object.assign(new Error(), {errors}))
+    return exec(schema, meString, {}, { user, ...ctx }).then(
+      ({ errors, data }) => {
+        if (errors) {
+          return Promise.reject(Object.assign(new Error(), { errors }))
+        }
         assert.equal(data.me.id, user.id)
         assert.equal(data.me.displayName, 'authorized user')
-      })
+      }
+    )
   })
   it('Upload file', () => {
     return req
       .post('/upload')
       .set('authorization', `Bearer ${generateUserToken(user)}`)
       .attach('upload', path.resolve(__dirname, 'image.png'))
-      .then(({status, body: {upload}}) => {
+      .then(({ status, body: { upload } }) => {
         assert.equal(status, 200)
         assert.equal(upload.user.id, user.id)
-        return req
-          .get(upload.url)
-          .then(({status, headers}) => {
-            assert.equal(status, 200)
-            assert.equal(headers['content-type'], 'application/octet-stream')
-          })
+        return req.get(upload.url).then(({ status, headers }) => {
+          assert.equal(status, 200)
+          assert.equal(headers['content-type'], 'application/octet-stream')
+        })
       })
   })
   it('View uploads', () => {
-    return exec(schema, uploadsString, {}, {user, ...ctx})
-      .then(({data: {uploads}}) => {
+    return exec(schema, uploadsString, {}, { user, ...ctx })
+      .then(({ data: { uploads } }) => {
         assert.equal(uploads.length, 0)
       })
       .then(() => {
@@ -95,8 +112,8 @@ describe('As authorized user', () => {
           .set('authorization', `Bearer ${generateUserToken(user)}`)
           .attach('upload', path.resolve(__dirname, 'image.png'))
       })
-      .then(() => exec(schema, uploadsString, {}, {user, ...ctx}))
-      .then(({data: {uploads}}) => {
+      .then(() => exec(schema, uploadsString, {}, { user, ...ctx }))
+      .then(({ data: { uploads } }) => {
         assert.equal(uploads[0].user.id, user.id)
       })
   })
